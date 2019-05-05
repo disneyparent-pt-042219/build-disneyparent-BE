89a9dcfec8982bb18b4c9a8ad3cd7df775818b31
erscriber <erscriber@gmail.com>
2019-05-05 01:01:41 -0400

    Testing boilerplate set up

---
+++ api/server.spec.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+
+const server = require('./server');
+
+describe(" ", () => {
+    it(' ', async() => {
+        const response = await request(server)
+        expect(response)
+    });
+});
