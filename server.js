e06ea1e62a7d909429ba0a4963d88fe5ec6d6e1c
erscriber <erscriber@gmail.com>
2019-05-06 15:53:06 -0400

    moved server from api tp app

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const configMiddleware = require('./middleware.js');
-const authRouter = require('../auth/authRouter.js');
-const familyRouter = require('../routers/familyRouter');
-const postsRouter = require('../routers/postsRouter');
-const commentRouter = require('../routers/commentRouter');
+const configMiddleware = require('./api/middleware.js');
+const authRouter = require('./auth/authRouter.js');
+const familyRouter = require('./routers/familyRouter');
+const postsRouter = require('./routers/postsRouter');
+const commentRouter = require('./routers/commentRouter');
 
 const server = express();
 
