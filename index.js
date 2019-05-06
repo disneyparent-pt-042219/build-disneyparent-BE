ba5723bfc2b0be9469c5a600b2df498e8f48e9e7
erscriber <erscriber@gmail.com>
2019-05-06 16:15:04 -0400

    refactor in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const server = require('./api/server.js');
+const server = require('./server');
 
 const port = process.env.PORT || 9090;
 server.listen(port, () => console.log(`\n** Server running on port ${port} **\n`));
