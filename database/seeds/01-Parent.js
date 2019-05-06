3f7caab40f56ed8ba35ba2b7912e1fce67834e43
erscriber <erscriber@gmail.com>
2019-05-06 15:39:54 -0400

    changed seed code

--- database/seeds/01-Parent.js
+++ database/seeds/01-Parent.js
@@ -3,7 +3,7 @@ const parentData = require('../data/parent_data');
 exports.seed = function(knex, Promise) {
     // Deletes ALL existing entries
     return knex('family')
-        .truncate()
+        .del()
         .then(function() {
             // Inserts seed entries
             return knex('family')
